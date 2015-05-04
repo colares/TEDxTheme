1a28ca4e59c696bffbd13040624cd49065ec8a4d
Thiago Colares <thiago@apimenti.com.br>
2015-05-04 20:43:10 -0300

    some partners fixes

--- shortcode_templates/partners_shortcode.php
+++ shortcode_templates/partners_shortcode.php
@@ -4,14 +4,16 @@
 		<?php if ($partners->have_posts()): while ($partners->have_posts()) : $partners->the_post(); ?>
 			<?php
 			if (has_post_thumbnail()):
-				$thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'small-partner');
+				$thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID));
+
+				//$thumb = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'small-partner');
 				if (is_array($thumb) && !empty($thumb[0])) {
 					$thumbnail_src = $thumb[0];
 				} else {
 					$thumbnail_src = '';
 				}
 				?>
-				<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 partner-tile">
+				<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 partner-tile" style="text-align: center">
 					<a href='<?= get_post_meta(get_the_ID(), '_partner_url', true); ?>' target="_blank">
 						<img alt='<?php the_title(); ?>' src='<?= $thumbnail_src; ?>'/>
 					</a>
