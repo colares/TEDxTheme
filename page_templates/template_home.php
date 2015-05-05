6dd42672007751fe1230b4e735f836b99773511d
Thiago Colares <thiago@apimenti.com.br>
2015-05-04 21:40:35 -0300

    removing blog from homepage

--- page_templates/template_home.php
+++ page_templates/template_home.php
@@ -20,17 +20,17 @@ get_header();
 
 <div class="container contents">
   <div class="row">
-    <div class="col-md-9">
+    <div class="col-md-12">
       <section>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <?php the_content(); ?>
         <?php endwhile; endif; ?>
       </section>
-		
+
     </div>
-    <div class="col-md-3">
-      <?php get_sidebar(); ?>
-    </div>
+<!--    <div class="col-md-3">-->
+      <?php //get_sidebar(); ?>
+<!--    </div>-->
   </div>
 </div><!-- .container contents -->
 <?php get_footer(); ?>
