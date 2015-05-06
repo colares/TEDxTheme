a990822208597d57d14609f96d1294cdd7996b0d
Thiago Colares <thiago@apimenti.com.br>
2015-05-05 21:25:45 -0300

    team: k.i.s.s.

--- shortcode_templates/team_members_shortcode.php
+++ shortcode_templates/team_members_shortcode.php
@@ -13,63 +13,26 @@
         $thumbnail_src = get_bloginfo('template_url') . "/images/defaults/team.jpg";
       }
       ?>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2"
+			 ng-init="descriptions[<?= $post->ID ?>] = {title: '<?= $post->post_title; ?>', content: '<?= $post->post_content; ?>', jobDescription: '<?= $teamMemberJobDescription ?>', twitter: '<?= $twitter; ?>' }"
+			>
+			<div class="thumbnail"
+				 style="border: 0; cursor: pointer;"
+				 ng-click="setModalDescriptionId(<?= $post->ID ?>)"
+				 data-toggle="modal" data-target="#descriptionModal"
+				>
+				<img
+					alt='<?php the_title(); ?>' src='<?=  $thumbnail_src; ?>'
+					style="position: relative; z-index: 1;" class="team-photo">
+				<div class="caption"
+					 style="cursor: pointer; background-color: #FF2B06; color: white"
+					 ng-click="setModalDescriptionId(<?= $post->ID ?>)"
+					 data-toggle="modal" data-target="#descriptionModal"
+					>
+					<h5><?=  $post->post_title; ?><br/><small style="color: white"><?=  get_post_meta($post->ID, '_team_job_description', true) ?></small></h5>
+				</div>
+			</div>
 
-      <div class="team-tile" data-remote="true"
-           data-href="<?php the_permalink(); ?>">
-		  <?php
-		  	$twitter = get_post_meta($post->ID, '_team_twitter_link', true);
-		  	$teamMemberJobDescription = get_post_meta($post->ID, '_team_job_description', true);
-		  ?>
-		  <div class="team-photo-wrapper"
-			   style="position: relative; cursor: pointer;"
-			   ng-click="setModalDescriptionId(<?= $post->ID ?>)"
-			   data-toggle="modal" data-target="#descriptionModal"
-			   ng-init="descriptions[<?= $post->ID ?>] = {title: '<?= $post->post_title; ?>', content: '<?= $post->post_content; ?>', jobDescription: '<?= $teamMemberJobDescription ?>', twitter: '<?= $twitter; ?>' }">
-			<i class="fa fa-2x fa-info-circle"
-			   style="position: absolute;right:10px; top: 10px;z-index: 10; color: white"
-				></i>
-		  	<img
-				alt='<?php the_title(); ?>' src='<?=  $thumbnail_src; ?>'
-				style="position: relative; z-index: 1;" class="team-photo">
-        </div>
-        <!-- .team-photo-wrapper -->
-        <div class="team-info">
-          <div class='team-links pull-right'>
-            <?php if (!empty($twitter)): ?>
-<!--              <div class='ir ico tweet ico-box'>-->
-			  <div>
-                <a href='<?=  $twitter; ?>' target='_blank' style="display:block; margin: 10px; z-index: 2000;">
-					<i class="fa fa-twitter fa-2x"></i>
-                </a>
-              </div>
-            <?php endif; ?>
-            <!-- END TODO -->
-          </div>
-          <!-- .team-links -->
-          <div class="team-title">
-            <h2
-				   style="position: relative; cursor: pointer;"
-				   ng-click="setModalDescriptionId(<?= $post->ID ?>)"
-				   data-toggle="modal" data-target="#descriptionModal"
-				   ng-init="descriptions[<?= $post->ID ?>] = {title: '<?= $post->post_title; ?>', content: '<?= $post->post_content; ?>', jobDescription: '<?= $teamMemberJobDescription ?>', twitter: '<?= $twitter; ?>' }"
-
-				><?=  $post->post_title; ?></h2>
-            <div class="team-role">
-              <?=  get_post_meta($post->ID, '_team_job_description', true) ?>
-            </div>
-          </div>
-          <!-- .team-title -->
-          <!--
-          <div class='team-details-container'>
-            <?=  $post->post_content; ?>
-          </div>-->
-          <!-- .team-details-container -->
-        </div>
-        <!-- .team-info -->
-
-
-      </div>
 		</div>
     <?php endwhile;
     endif;
@@ -90,8 +53,7 @@
 					</h3>
 					<p class="text-muted"><em>{{descriptions[modalDescriptionId].jobDescription}}</em></p>
 					<p>{{descriptions[modalDescriptionId].content}}</p>
-					<p>
-
+					<p ng-show="descriptions[modalDescriptionId].twitter != null">
 						<a href='{{descriptions[modalDescriptionId].twitter}}' target='_blank'>
 							<i class="fa fa-twitter"></i> {{descriptions[modalDescriptionId].twitter}}
 						</a>
